e55e27b9c638d373ae19b9c0dab1014eeb949921
Yaroslav <yaroslav.solodovnikov@geeksforless.com>
2023-09-18 12:49:27 +0100

    Get users list

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,2 +1,3 @@
 export const USERS_LIST_KEY = 'users'
 export const DEFAULT_ALERT_MSG = 'Operation was successful'
+export const DEFAULT_USERS_COUNT_BY_REQUEST = 20
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
-import { Coordinates } from '../user-card/user-card';
+import { Component, OnInit } from '@angular/core';
+import { Coordinates, UserCard } from '../user-card/user-card';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+  users: Array<UserCard> = []
   coordinates: Coordinates = {
     latitude: '0',
     longitude: '0'
   }
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUser().subscribe((user) => {
+      this.users = user.results
+    })
+  }
+
+
   getWeather(coordinates: Coordinates) {
     this.coordinates = coordinates
+    const el = document.getElementById('navbar')
+    el?.scrollIntoView()
   }
 }
